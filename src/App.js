34bc7d609c5e5154c4dc5ef981d04d431e478adb
junseokJang <89775772+junseokJang@users.noreply.github.com>
2021-09-15 10:08:20 +0900

    v0.0.2.5j

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
+import { render } from "@testing-library/react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useEffect } from 'react';
-import { Route, Switch, useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Route, Switch, useLocation, Redirect } from 'react-router-dom';
 import './App.scss';
 import NotFound from './components/404/NotFound';
 import Create from "./components/Create/Create";
@@ -15,34 +16,49 @@ import Profile from "./components/Profile/Profile";
 import Ranking from "./components/Ranking/Ranking";
 import SubNav from './components/SubNav/SubNav';
 
-
 export default function App() {
-  const location = useLocation();
-  useEffect(()=>{
 
-      
-  },[location])
+  const location = useLocation();
+  console.log("location : " +location)
+  const [checkLogin, setCheckLogin] = useState(false)
+  const check =(history)=> async () => {
+    setCheckLogin(true)
+    fetch("https://localhost:8443/loginform").then(async response => {
+      const answer = await response.text()
+      console.log("answer : "+answer)
+      if (answer == "loginform") {
+          return (history.push("/login")
+          )
+      }
+    })
+  }
+
+  useEffect(() => {
+    if (checkLogin)
+      setCheckLogin(false)
+
+  }, [location])
   return (
     <div className="App">
       <Nav />
       <SubNav />
-      <div className="main-section" 
+      <div className="main-section"
       >
-        
+
         <Switch >
-          <Route exact path="/" component={Main} />
-          <Route exact path="/login" component={Login}/>
-          <Route exact path="/detail" component={Detail}/>
-          <Route exact path="/ranking" component={Ranking}/>
-          <Route exact path="/profile" component={Profile}/>
-          <Route exact path="/mypage" component={MyPage}/>
-          <Route exact path="/create" component={Create}/>
-          <Route exact path="/list" component={List}/>
+          <Route exact path="/" render={props => <Main check={check} />} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/detail" component={Detail} />
+          <Route exact path="/ranking" component={Ranking} />
+          <Route exact path="/profile" component={Profile} />
+          <Route exact path="/mypage" component={MyPage} />
+          <Route exact path="/create" component={Create} />
+          <Route exact path="/list" component={List} />
           <Route component={NotFound} />
         </Switch>
       </div>
       <Footer />
-      </div>
+    </div>
   );
 }
 
