313c8c7a983f28daef19272a787a13b8da6a9e12
junseokJang <89775772+junseokJang@users.noreply.github.com>
2021-09-23 08:35:09 +0900

    v.0.0.0.5jjs

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,9 +1,20 @@
+import { useEffect,useState } from "react"
 import "./List.scss"
+import ImgBox from "../ImgBox/ImgBox";
 
-export default function List() {
+export default function List(props) {
+    const [data, setData] = useState(null);
+
+
+    useEffect(()=>{
+        console.log(props.match.path)
+        setData()
+    })
     return(
         <div className="List">
-                Im list Page
+            <div className="list-section">
+                
             </div>
-            )
+        </div>
+    )
 };
